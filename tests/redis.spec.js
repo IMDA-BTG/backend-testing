8e90db022b1fb0682baeb86b2c9fccb1f3298194
Benedict Lee <benedict_lee@imda.gov.sg>
2023-07-28 10:33:27 +0800

    fix(redis.spec.js): Fixed Base Dir Variables

--- tests/redis.spec.js
+++ tests/redis.spec.js
@@ -21,19 +21,13 @@ test.describe('Test Engine Task', () => {
             "algorithmArgs": {},
             "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelFile": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav",
-            "groundTruthDataset":baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType":"classification",
             "groundTruth":"Interest_Rate"
         })
 
         // Create Connection to App via Redis
         await connection.connect()
-
-        // Register Plugin
-        await connection.hSet("algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot", "inputSchema", "{\"$schema\":\"https://json-schema.org/draft/2020-12/schema\",\"$id\":\"/partial_dependence_plot/input.schema.json\",\"title\":\"Algorithm Plugin Input Arguments\",\"description\":\"A schema for algorithm plugin input arguments\",\"type\":\"object\",\"required\":[\"target_feature_name\",\"percentiles\",\"grid_resolution\"],\"properties\":{\"target_feature_name\":{\"title\":\"Target Feature Name\",\"description\":\"Target Feature Name (e.g. Interest_Rate)\",\"type\":\"string\"},\"percentiles\":{\"title\":\"Cut-off percentiles\",\"description\":\"Cut-off percentiles (e.g. [0.01, 0.99])\",\"type\":\"array\",\"minItems\":2,\"maxItems\":2,\"items\":{\"type\":\"number\"}},\"grid_resolution\":{\"title\":\"Grid Resolution\",\"description\":\"Grid Resolution (e.g. 25)\",\"type\":\"number\"}}}")
-        await connection.hSet("algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot", "requirements", "[\"numpy==1.23.5 ; python_version >= \\\"3.10\\\" and python_version < \\\"3.11\\\"\",\"scipy==1.10.1 ; python_version >= \\\"3.10\\\" and python_version < \\\"3.11\\\"\"]")
-        await connection.hSet("algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot", "outputSchema", "{\"$schema\":\"https://json-schema.org/draft/2020-12/schema\",\"$id\":\"/partial_dependence_plot/output.schema.json\",\"title\":\"Algorithm Plugin Output Arguments\",\"description\":\"A schema for algorithm plugin output arguments\",\"type\":\"object\",\"required\":[\"feature_names\",\"results\"],\"properties\":{\"feature_names\":{\"type\":\"array\",\"description\":\"Array of feature names\",\"minItems\":1,\"items\":{\"type\":\"string\"}},\"output_classes\":{\"description\":\"Array of output classes\",\"type\":\"array\",\"minItems\":1,\"items\":{\"type\":[\"string\",\"number\",\"integer\",\"boolean\"]}},\"results\":{\"description\":\"Matrix of feature values (# feature names)\",\"type\":\"array\",\"minItems\":1,\"items\":{\"description\":\"Matrix of PDP plot data (# output classes)\",\"type\":\"array\",\"minItems\":1,\"items\":{\"type\":\"array\",\"description\":\"Array of PDP values for each feature value (# feature values)\",\"minItems\":1,\"items\":{\"type\":\"object\",\"description\":\"Array of feature and PDP value\",\"required\":[\"feature_value\",\"pdp_value\"],\"properties\":{\"feature_value\":{\"type\":\"number\"},\"pdp_value\":{\"type\":\"number\"}}}}}}}}")
-        await connection.hSet("algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot", "data", "{\"cid\":\"partial_dependence_plot\",\"name\":\"partial dependence plot\",\"modelType\":[\"classification\",\"regression\"],\"version\":\"0.1.0\",\"author\":\"IMDA-T2E\",\"description\":\"A Partial Dependence Plot (PDP) explains how each feature and its feature value contribute to the predictions.\",\"tags\":[\"partial dependence plot\",\"classification\",\"regression\"],\"requireGroundTruth\":false,\"requiredFiles\":[\"AUTHORS.rst\",\"CHANGELOG.md\",\"input.schema.json\",\"LICENSE\",\"output.schema.json\",\"partial_dependence_plot.meta.json\",\"partial_dependence_plot.py\",\"README.md\",\"requirements.txt\",\"syntax_checker.py\"],\"type\":\"Algorithm\",\"gid\":\"aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot\",\"pluginGID\":\"aiverify.stock.algorithms.partial_dependence_plot\",\"algoPath\":\"" + pluginFilePath + "/ai-verify-portal/plugins/partial-dependence-plot\"}")
 
         // Open API Connection to App via Redis
         const client = await new Client().use(connection)
@@ -69,8 +63,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30024",
             "algorithmId": "algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot",
             "algorithmArgs": { "percentiles": [0.05, 0.95], "target_feature_name": "Interest_Rate", "grid_resolution": 100 },
-            "testDataset": baseDir + "/uploads/data/combine_all.sh",
-            "modelFile": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav",
+            "testDataset": baseDir + "/backend-testing/uploads/data/combine_all.sh",
+            "modelFile": baseDir + "/backend-testing/uploads/model/pickle_scikit_multiclasslr_loan.sav",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })    
@@ -108,8 +103,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30025",
             "algorithmId": "algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot",
             "algorithmArgs": { "percentiles": [0.05, 0.95], "target_feature_name": "Interest_Rate", "grid_resolution": 100 },
-            "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
-            "modelFile": baseDir + "/uploads/data/combine_all.sh",
+            "testDataset": baseDir + "/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "modelFile": baseDir + "/backend-testing/uploads/data/combine_all.sh",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })
@@ -147,9 +143,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30026",
             "algorithmId": "algo:aiverify.stock.algorithms.fairness_metrics_toolbox:fairness_metrics_toolbox",
             "algorithmArgs": { "sensitive_feature": ["Gender", "Home_Owner"] },
-            "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
-            "modelFile": baseDir + "/uploads/model/combine_all.sh",
-            "groundTruthDataset": baseDir + "/pickle_pandas_tabular_loan_testing.sav",
+            "testDataset": baseDir + "/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "modelFile": baseDir + "/backend-testing/uploads/model/combine_all.sh",
+            "groundTruthDataset": baseDir + "/backend-testing/pickle_pandas_tabular_loan_testing.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })
@@ -187,8 +183,8 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30027",
             "algorithmId": "aiverify.algorithms.partial_dependence_plot:partial_dependence_plot",
             "algorithmArgs": { "percentiles": [0.05, 0.95], "target_feature_name": "Interest_Rate", "grid_resolution": 100 },
-            "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
-            "modelFile": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav",
+            "testDataset": baseDir + "/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "modelFile": baseDir + "/backend-testing/uploads/model/pickle_scikit_multiclasslr_loan.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })
@@ -227,7 +223,7 @@ test.skip('Test Engine Service', () => {
 
         validateDataset = JSON.stringify({
             "serviceId": "service:64530",
-            "filePath": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav"
+            "filePath": baseDir + "/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav"
         })
 
         // Create Connection to App via Redis
@@ -258,7 +254,7 @@ test.skip('Test Engine Service', () => {
 
         validateDataset = JSON.stringify({
             "serviceId": "service:64531",
-            "filePath": baseDir + "/uploads/data/combine_all.sh"
+            "filePath": baseDir + "/backend-testing/uploads/data/combine_all.sh"
         })
 
         // Create Connection to App via Redis
@@ -291,7 +287,7 @@ test.skip('Test Engine Service', () => {
         validateModel = JSON.stringify({
             "serviceId": "service:64530a39dc46da5656d1593k",
             "mode": "upload",
-            "filePath": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav"
+            "filePath": baseDir + "/backend-testing/uploads/model/pickle_scikit_multiclasslr_loan.sav"
         })
 
         // Create Connection to App via Redis
