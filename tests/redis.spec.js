44a074f91c7f6afa36a5d3bf8c10292517c16373
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2023-08-22 21:52:54 +0800

    - create UPLOADSDIR env variable for portal uploads dir - copy test files to portal uploads dir (for docker instance) - remove unused dotenv

--- tests/redis.spec.js
+++ tests/redis.spec.js
@@ -1,16 +1,32 @@
 import { Client } from 'redis-om'
 import { createClient } from 'redis'
 import { test, expect } from '@playwright/test'
-import dotenv from 'dotenv';
+import * as fs from "fs"
+//import dotenv from 'dotenv'
 
 const URI = 'redis://127.0.0.1:6379'
 const connection = createClient({ URI })
 
-let task, validateDataset, validateModel, baseDir = process.env.BASEDIR
+let task, validateDataset, validateModel
+
+// baseDir = aiverify repo base dir
+const baseDir = process.env.BASEDIR ?? "/app/aiverify"
+// testFileDir = test files folder in backend-testing repo
+let testFileDir = "./uploads"
+// Portal uploads folder
+let uploadsDir = process.env.UPLOADSDIR
 
 test.describe.configure({ mode: 'serial' });
 
 test.describe('Test Engine Task', () => {
+
+    test.beforeAll(() => {
+        // Copy test files to portal uploads folder
+        if (uploadsDir) {
+            console.log("Copy test files to portal uploads dir...")
+            fs.cpSync(testFileDir, uploadsDir, { recursive: true })
+        }
+    })
 
     test('Test Engine Task with Valid Inputs (Upload)', async () => {
 
@@ -19,9 +35,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30023",
             "algorithmId": "algo:aiverify.stock.partial_dependence_plot:partial_dependence_plot",
             "algorithmArgs": {},
-            "testDataset": baseDir + "/qa-test/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
-            "modelFile": baseDir + "/qa-test/backend-testing/uploads/model/pickle_scikit_multiclasslr_loan.sav",
-            "groundTruthDataset": baseDir + "/qa-test/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "modelFile": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType":"classification",
             "groundTruth":"Interest_Rate"
         })
@@ -63,9 +79,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30024",
             "algorithmId": "algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot",
             "algorithmArgs": { "percentiles": [0.05, 0.95], "target_feature_name": "Interest_Rate", "grid_resolution": 100 },
-            "testDataset": baseDir + "/qa-test/backend-testing/uploads/data/combine_all.sh",
-            "modelFile": baseDir + "/qa-test/backend-testing/uploads/model/pickle_scikit_multiclasslr_loan.sav",
-            "groundTruthDataset": baseDir + "/qa-test/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "testDataset": baseDir + "/uploads/data/combine_all.sh",
+            "modelFile": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })    
@@ -103,9 +119,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30025",
             "algorithmId": "algo:aiverify.stock.algorithms.partial_dependence_plot:partial_dependence_plot",
             "algorithmArgs": { "percentiles": [0.05, 0.95], "target_feature_name": "Interest_Rate", "grid_resolution": 100 },
-            "testDataset": baseDir + "/qa-test/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
-            "modelFile": baseDir + "/qa-test/backend-testing/uploads/data/combine_all.sh",
-            "groundTruthDataset": baseDir + "/qa-test/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "modelFile": baseDir + "/uploads/data/combine_all.sh",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })
@@ -143,9 +159,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30026",
             "algorithmId": "algo:aiverify.stock.algorithms.fairness_metrics_toolbox:fairness_metrics_toolbox",
             "algorithmArgs": { "sensitive_feature": ["Gender", "Home_Owner"] },
-            "testDataset": baseDir + "/qa-test/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
-            "modelFile": baseDir + "/qa-test/backend-testing/uploads/model/combine_all.sh",
-            "groundTruthDataset": baseDir + "/qa-test/backend-testing/pickle_pandas_tabular_loan_testing.sav",
+            "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "modelFile": baseDir + "/uploads/model/combine_all.sh",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })
@@ -183,9 +199,9 @@ test.describe('Test Engine Task', () => {
             "id": "task:642691211b68cd044de3001e-642691211b68cd044de30027",
             "algorithmId": "aiverify.algorithms.partial_dependence_plot:partial_dependence_plot",
             "algorithmArgs": { "percentiles": [0.05, 0.95], "target_feature_name": "Interest_Rate", "grid_resolution": 100 },
-            "testDataset": baseDir + "/qa-test/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav",
-            "modelFile": baseDir + "/qa-test/backend-testing/uploads/model/pickle_scikit_multiclasslr_loan.sav",
-            "groundTruthDataset": baseDir + "/qa-test/backend-testing/pickle_pandas_tabular_loan_testing.sav",
+            "testDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
+            "modelFile": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav",
+            "groundTruthDataset": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav",
             "modelType": "classification",
             "groundTruth": "Interest_Rate"
         })
@@ -224,7 +240,7 @@ test.skip('Test Engine Service', () => {
 
         validateDataset = JSON.stringify({
             "serviceId": "service:64530",
-            "filePath": baseDir + "/qa-test/backend-testing/uploads/data/pickle_pandas_tabular_loan_testing.sav"
+            "filePath": baseDir + "/uploads/data/pickle_pandas_tabular_loan_testing.sav"
         })
 
         // Create Connection to App via Redis
@@ -255,7 +271,7 @@ test.skip('Test Engine Service', () => {
 
         validateDataset = JSON.stringify({
             "serviceId": "service:64531",
-            "filePath": baseDir + "/qa-test/backend-testing/uploads/data/combine_all.sh"
+            "filePath": baseDir + "/uploads/data/combine_all.sh"
         })
 
         // Create Connection to App via Redis
@@ -288,7 +304,7 @@ test.skip('Test Engine Service', () => {
         validateModel = JSON.stringify({
             "serviceId": "service:64530a39dc46da5656d1593k",
             "mode": "upload",
-            "filePath": baseDir + "/qa-test/backend-testing/uploads/model/pickle_scikit_multiclasslr_loan.sav"
+            "filePath": baseDir + "/uploads/model/pickle_scikit_multiclasslr_loan.sav"
         })
 
         // Create Connection to App via Redis
